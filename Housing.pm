11e6c473069a4242e5d6ed8bdf7bae922dddafc1
Glenn Wood <glenwood@alumni.caltech.edu>
2003-01-20 18:54:10 +0000

    Import of GLENNWOOD/Bundle-WWW-Scraper-Housing-0.02 from CPAN.
    gitpan-cpan-distribution: Bundle-WWW-Scraper-Housing
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         GLENNWOOD/Bundle-WWW-Scraper-Housing-0.02.tar.gz
    gitpan-cpan-author:       GLENNWOOD
    gitpan-cpan-maturity:     released

---
+++ Housing.pm
@@ -0,0 +1,26 @@
+package Bundle::WWW::Search::Scraper::Housing;
+
+$VERSION = '0.02';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::WWW::Search::Scraper::Housing - Snapshot of installation on  on Sat Jan 18 01:45:17 2003
+
+=head1 SYNOPSIS
+
+perl -MCPAN -e 'install Bundle::WWW::Search::Scraper::Housing'
+
+=head1 CONTENTS
+
+Bundle::WWW::Search::Scraper::Housing
+
+WWW::Search::Scraper::Apartments
+
+
+=head1 AUTHOR
+
+Glenn Wood, http://search.cpan.org/search?mode=author&query=GLENNWOOD.
